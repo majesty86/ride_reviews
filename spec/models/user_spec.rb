9dcea8c020d3179d9066ae790e13ce9f808cc148
majesty86 <tladislas@gmail.com>
2017-12-19 17:45:00 -0600

    DRY refactor, add association test.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,40 +1,44 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
+describe User, type: :model do
   let!(:user) { User.create!(username: "jseinfeld",
                         email: "jseinfeld@hello.com",
                         password: "helloworld"
                         )}
 
-  subject { described_class.new }
+  subject { described_class.new(username: "gconstanza",
+                                email: "gconstanza@hello.com",
+                                password: "helloworld"
+                                )}
 
   it 'is valid with valid attributes' do 
     expect(user).to be_valid
   end
 
   it 'is not valid without a username' do 
-    subject.email    = "gconstanza@hello.com"
-    subject.password = "helloworld"
-    expect(subject).to_not be_valid
+    user.username = nil
+    expect(user).to_not be_valid
   end
 
   it 'is not valid without an email' do 
-    subject.username = "gconstanza"
-    subject.password = "helloworld"
-    expect(subject).to_not be_valid
+    user.email = nil
+    expect(user).to_not be_valid
   end
 
   it 'is not valid if username is already taken' do 
     subject.username = "jseinfeld"
-    subject.email    = "gconstanza@hello.com"
-    subject.password = "helloworld"
     expect(subject).to_not be_valid
   end
 
   it 'is not valid if email is already taken' do 
-    subject.username = "gconstanza"
     subject.email    = "jseinfeld@hello.com"
-    subject.password = "helloworld"
     expect(subject).to_not be_valid
+  end
+end
+
+describe "Associations" do
+  it 'can be an author of many rides' do 
+    association = User.reflect_on_association(:rides)
+    expect(association.macro).to eq :has_many
   end
 end
