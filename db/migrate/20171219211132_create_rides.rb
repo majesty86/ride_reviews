c53041bf205610b5e6079384dc46229c04d44b96
majesty86 <tladislas@gmail.com>
2017-12-19 15:37:51 -0600

    Update migration references.

--- db/migrate/20171219211132_create_rides.rb
+++ db/migrate/20171219211132_create_rides.rb
@@ -7,7 +7,7 @@ class CreateRides < ActiveRecord::Migration[5.1]
       t.string :ending_intersection
       t.integer :distance_in_miles
       t.integer :star_rating
-      t.references :author_id, foreign_key: true
+      t.references :user, foreign_key: :author_id
 
       t.timestamps
     end
