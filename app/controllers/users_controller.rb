7c438f7ab9913ecf1d4a8a941aadea006ebe1235
majesty86 <tladislas@gmail.com>
2017-12-30 11:16:35 -0600

    Add sessions and users controllers.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,38 @@
+class UsersController
+  
+  def new
+    
+    respond_to do |f|
+      f.js
+    end
+
+    @user = User.new
+  end
+
+  def create
+
+    respond_to do |f|
+      f.js
+    end
+
+    @user = User.new(user_params)
+
+    if @user.save
+      session[:user_id] = @user.id
+      render :create
+    else
+      @errors = @user.errors.full_messages
+      render :new
+    end
+    
+  end
+
+  def show
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:username, :email, :password)
+    end
+
+end
