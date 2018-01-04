a5ae74666956f3f4793b14b6edaf54ead143ef38
majesty86 <tladislas@gmail.com>
2018-01-04 00:02:28 -0600

    Add methods, inherit from Application Controller.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,37 @@
-class SessionsController
+class SessionsController < ApplicationController
   include SessionHelper
 
+  def new
+    @user = User.new
+
+    respond_to do |f|
+      f.html
+      f.js
+    end
+
+  end
+
+  def create
+
+    respond_to do |f|
+      f.html
+      f.js
+    end
+    
+    @user = User.find_by(email: params[:email])
+
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      render :create
+    else
+      @errors = ["Login failed."]
+      render :new
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path
+  end
   
 end
