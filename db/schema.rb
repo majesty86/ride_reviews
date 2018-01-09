c425532426f9ee2996170820aa8d8bb1ae3a9672
majesty86 <tladislas@gmail.com>
2018-01-09 12:13:31 -0600

    Update migrations.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171219211132) do
+ActiveRecord::Schema.define(version: 20180109174543) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(version: 20171219211132) do
     t.bigint "author_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "starting_state"
+    t.string "ending_state"
     t.index ["author_id"], name: "index_rides_on_author_id"
   end
 
