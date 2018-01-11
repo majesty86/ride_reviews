0ca8339a34009ce238fa40eb3b1a75dee9ef7e79
majesty86 <tladislas@gmail.com>
2018-01-11 00:42:30 -0600

    Add images.

--- app/assets/javascripts/rides.js
+++ app/assets/javascripts/rides.js
@@ -9,3 +9,14 @@ var titleHoverOnHandler = function(){
 var titleHoverOffHandler = function(){
   $(this).css('color', 'white')
 };
+
+// For image slideshows:
+  // Build image array
+  // Time the change through the JS here with some fades
+  // Function for the div will be something like:
+    // $(this).css('background-image', 'image-url(image_array.sample)')
+  // Just execute function on bottom?  How to repeat?
+
+
+
+
