ff0d1bc48f8afd484b7667517620ced97d9443dc
majesty86 <tladislas@gmail.com>
2018-01-16 01:17:00 -0600

    Write profanity filter.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -11,5 +11,45 @@ class Ride < ApplicationRecord
   validates :distance_in_miles, presence: true
   validates :star_rating, presence: true
   validates_inclusion_of :star_rating, in: 1..5, message: "must be between 1 and 5"
+  validate :new_ride_profanity_filter
 
+  def new_ride_profanity_filter
+    title_profanity_filter &&
+    address_profanity_filter &&
+    city_profanity_filter
+  end
+
+
+  private
+    def title_profanity_filter
+      profanity_filter(title, :title)
+    end
+
+    def address_profanity_filter
+      profanity_filter(starting_address, :starting_address) && 
+      profanity_filter(ending_address, :ending_address)
+    end
+
+    def city_profanity_filter
+      profanity_filter(starting_city, :starting_city) &&
+      profanity_filter(ending_city, :ending_city)
+    end
+
+    def profanity_filter(column_name, column_symbol)
+      split_words(column_name).each do |word|
+        add_language_errors(column_symbol) if inappropriate_words.include?(word.downcase)
+      end
+    end
+
+    def split_words(column_name)
+      column_name.split
+    end
+
+    def add_language_errors(column_name)
+      errors.add(column_name, 'must contain appropriate language')
+    end
+
+    def inappropriate_words
+      %w(ass bitch retard shit piss cunt fuck cocksucker motherfucker tits fart turd twat)
+    end
 end
