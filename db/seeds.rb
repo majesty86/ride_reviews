875f25c6ddd508ed7c4a62c74d79827cbc792bd7
majesty86 <tladislas@gmail.com>
2018-04-23 23:18:52 -0500

    Modify seedfile with rides.

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,17 +3,6 @@ User.create!(username: 'jseinfeld',
              password: 'helloworld'
              )
 
-Ride.create!(title: "Willis Tower to Revzilla",
-             starting_city: 'Chicago',
-             starting_state: 'IL',
-             ending_city: 'Philadelphia',
-             ending_state: 'PA',
-             starting_address: '233 S Wacker Dr.',
-             ending_address: '4020 S 26th St.',
-             distance_in_miles: 760,
-             star_rating: 5,
-             author_id: 1)
-
 Ride.create!(title: "Tail of the Dragon",
              starting_city: "Robbinsville",
              starting_state: "NC",
@@ -24,3 +13,115 @@ Ride.create!(title: "Tail of the Dragon",
              distance_in_miles: 11,
              star_rating: 5,
              author_id: 1)
+
+Ride.create!(title: "Twisted Sisters",
+             starting_city: "Camp Wood",
+             starting_state: "TX",
+             ending_city: "Medina",
+             ending_state: "TX",
+             starting_address: "103 Nueces St.",
+             ending_address: "14024 TX-16",
+             distance_in_miles: 57,
+             star_rating: 5,
+             author_id: 1)
+
+Ride.create!(title: "Beartooth Highway",
+             starting_city: "Cody",
+             starting_state: "MT",
+             ending_city: "Red Lodge",
+             ending_state: "MT",
+             starting_address: "708 US-212",
+             ending_address: "400 Bear Creek Hill",
+             distance_in_miles: 111,
+             star_rating: 5,
+             author_id: 1)
+
+Ride.create!(title: "Blue Ridge Parkway",
+             starting_city: "Bryson City",
+             starting_state: "NC",
+             ending_city: "Lyndhurst",
+             ending_state: "VA",
+             starting_address: "245 Fryemont St",
+             ending_address: "45 Royal Oaks Ln",
+             distance_in_miles: 377,
+             star_rating: 5,
+             author_id: 1)
+
+Ride.create!(title: "Pacific Coast Highway",
+             starting_city: "Granada Hills",
+             starting_state: "CA",
+             ending_city: "Mendocino",
+             ending_state: "CA",
+             starting_address: "13698 Balboa Blvd",
+             ending_address: "569-10507 Evergreen St",
+             distance_in_miles: 570,
+             star_rating: 5,
+             author_id: 1)
+
+Ride.create!(title: "Skyline Drive",
+             starting_city: "Afton",
+             starting_state: "VA",
+             ending_city: "Stanardsville",
+             ending_state: "VA",
+             starting_address: "3278 Afton Mountain Road",
+             ending_address: "524 Spotswood Trail",
+             distance_in_miles: 42,
+             star_rating: 5,
+             author_id: 1)
+
+Ride.create!(title: "Pig Trail Scenic Byway",
+             starting_city: "Ozark",
+             starting_state: "AR",
+             ending_city: "St Paul",
+             ending_state: "AR",
+             starting_address: "11594-11606 AR-23",
+             ending_address: "481 5th St",
+             distance_in_miles: 24,
+             star_rating: 5,
+             author_id: 1)
+
+Ride.create!(title: "San Juan Skyway",
+             starting_city: "Durango",
+             starting_state: "CO",
+             ending_city: "Silverton",
+             ending_state: "CO",
+             starting_address: "299-101 Cypress Ct",
+             ending_address: "809 Reese St",
+             distance_in_miles: 40,
+             star_rating: 5,
+             author_id: 1)
+
+Ride.create!(title: "Natchez Trace Parkway",
+             starting_city: "Nashville",
+             starting_state: "TN",
+             ending_city: "Natchez",
+             ending_state: "MS",
+             starting_address: "8400 TN-100",
+             ending_address: "1-7 Etania St",
+             distance_in_miles: 463,
+             star_rating: 5,
+             author_id: 1)
+
+Ride.create!(title: "Going-to-the-Sun Road",
+             starting_city: "St Mary",
+             starting_state: "MT",
+             ending_city: "West Glacier",
+             ending_state: "MT",
+             starting_address: "3 Going-to-the-Sun Rd",
+             ending_address: "196 Apgar Loop",
+             distance_in_miles: 48,
+             star_rating: 5,
+             author_id: 1)
+
+Ride.create!(title: "Tunnel of Trees",
+             starting_city: "Harbor Springs",
+             starting_state: "MI",
+             ending_city: "Harbor Springs",
+             ending_state: "MI",
+             starting_address: "542 State St",
+             ending_address: "6351 N State Rd",
+             distance_in_miles: 15,
+             star_rating: 5,
+             author_id: 1)
+
+
